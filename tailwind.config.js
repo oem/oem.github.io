e2bc2c06e9e92f99917d457d71405247ab07fa57
Ömür Özkir <oemuer.oezkir@gmail.com>
2021-04-09 21:10:01 +0200

    being silly with colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@ module.exports = {
   purge: ["./src/**/*.html", "./src/**/*.js", "./src/**/*.elm"],
   darkMode: "media", // or 'media' or 'class'
   theme: {
-    extend: {}
+    extend: {
+      keyframes: {
+        bgpulse: {
+          "0%, 100%": { background: "#003d4d" },
+          "50%": { background: "#004a4d" }
+        }
+      },
+      animation: {
+        bgpulse: "bgpulse 9s infinite"
+      },
+      colors: {
+        ultramarine: {
+          light: "#004a4d",
+          DEFAULT: "#003d4d",
+          dark: "#003d4d"
+        }
+      }
+    }
   },
   variants: {
     extend: {}
